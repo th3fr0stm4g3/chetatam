3d47b2933951d1459456ce5e280b14da089b3d51
th3fr0stm4g3 <mrfayrick@gmail.com>
2025-03-01 14:36:10 +0700

    лабы закинул

---
+++ ConsoleApplication1/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    double x, y, z;
+    cout << "Введите 3 переменных (через enter): ";
+    cin >> x >> y >> z;
+    
+    if (1 >= x or y or z <= 3)
+    {
+        cout << x or y or z; "true";
+    }
+    else
+    {
+        cout << "false";
+    }
+
+}
---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUSSIAN");
+
+    double x, y, z;
+    cout << "Введите 3 переменных (через enter): ";
+    cin >> x >> y >> z;
+
+    if (1 <= x && x >= 3)
+    {
+        cout << x << " x - принадлежит диапазону" << endl; 
+    }
+    if (1 <= y && y >= 3)
+    {
+        cout << y << " y - принадлежит диапазону" << endl;
+    }
+    if (1 <= z && z >= 3)
+    {
+        cout << z << " z - принадлежит диапазону" << endl;
+    }
+
+}
